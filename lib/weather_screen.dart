0d44c66c08f3e3b91399359468049197a94da1fa
accdev96 <andrescalzado41@gmail.com>
2025-03-21 21:24:22 +0100

    Update weather_screen.dart

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -15,33 +15,25 @@ class _WeatherScreenState extends State<WeatherScreen> {
   String _city = '';
   List<Map<String, dynamic>> _forecast = [];
   String _error = '';
-  LatLng _cityLatLng = LatLng(0.0, 0.0); // Coordenadas predeterminadas (lat, lng)
+  LatLng _cityLatLng = LatLng(
+    0.0,
+    0.0,
+  ); // Coordenadas predeterminadas (lat, lng)
+  int _selectedIndex = 0; // Para controlar el índice del BottomNavigationBar
 
-String _normalizeCityName(String city) {
-  Map<String, String> replacements = {
-    'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'ñ': 'n'
-  };
-  city = city.toLowerCase(); // Convertimos a minúsculas para uniformidad
-  replacements.forEach((key, value) {
-    city = city.replaceAll(key, value);
-  });
-  return city;
-}
-
-
+  // Métodos para obtener el pronóstico por horas y por el día
   Future<void> _fetchWeather() async {
-    final String apiKey = '1bacfbd7cde7607f9441c8e0c8d09a69'; // Sustituir con tu clave API
-    final String cityNormalized = _normalizeCityName(_city);
-
-final String url = 'https://api.openweathermap.org/data/2.5/forecast?q=$cityNormalized,ES&appid=$apiKey&units=metric&lang=es';
-final response = await http.get(Uri.parse(url));
-print(response.body);  // Ver la respuesta completa para detectar posibles errores
-
+    final String apiKey =
+        '1bacfbd7cde7607f9441c8e0c8d09a69'; // Sustituir con tu clave API
+    //final String cityNormalized = _normalizeCityName(_city);
+    final String url =
+        'https://api.openweathermap.org/data/2.5/forecast?q=$_city,ES&appid=$apiKey&units=metric&lang=es';
 
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
+        print(response.body);
 
         List<Map<String, dynamic>> dailyForecast = [];
         Map<String, dynamic> dailyData = {};
@@ -49,7 +41,9 @@ print(response.body);  // Ver la respuesta completa para detectar posibles error
         var dateFormat = DateFormat('dd/MM/yyyy');
 
         for (var entry in data['list']) {
-          final DateTime dt = DateTime.fromMillisecondsSinceEpoch(entry['dt'] * 1000);
+          final DateTime dt = DateTime.fromMillisecondsSinceEpoch(
+            entry['dt'] * 1000,
+          );
           final date = dateFormat.format(dt);
 
           Map<String, String> weekDaysTranslation = {
@@ -64,14 +58,18 @@ print(response.body);  // Ver la respuesta completa para detectar posibles error
 
           if (!dailyData.containsKey(date)) {
             String dayOfWeekEnglish = DateFormat('EEEE').format(dt);
-            String dayOfWeekSpanish = weekDaysTranslation[dayOfWeekEnglish] ?? dayOfWeekEnglish;
+            String dayOfWeekSpanish =
+                weekDaysTranslation[dayOfWeekEnglish] ?? dayOfWeekEnglish;
 
             dailyData[date] = {
               'date': date,
               'dayOfWeek': dayOfWeekSpanish,
               'temp': entry['main']['temp'],
               'description': entry['weather'][0]['description'],
-              'rain': entry['rain'] != null ? (entry['rain']['3h'] as num).toDouble() : 0.0,
+              'rain':
+                  entry['rain'] != null
+                      ? (entry['rain']['3h'] as num).toDouble()
+                      : 0.0,
               'icon': entry['weather'][0]['icon'],
             };
           }
@@ -79,15 +77,22 @@ print(response.body);  // Ver la respuesta completa para detectar posibles error
 
         dailyForecast = List<Map<String, dynamic>>.from(dailyData.values);
 
-        double lat = data['city']['coord']['lat'];
-        double lon = data['city']['coord']['lon'];
+        double lat =
+            (data['city']['coord']['lat'] is int)
+                ? (data['city']['coord']['lat'] as int).toDouble()
+                : data['city']['coord']['lat'];
+
+        double lon =
+            (data['city']['coord']['lon'] is int)
+                ? (data['city']['coord']['lon'] as int).toDouble()
+                : data['city']['coord']['lon'];
+
         _cityLatLng = LatLng(lat, lon);
 
         setState(() {
           _forecast = dailyForecast;
-          _error = '';  
+          _error = '';
         });
-
       } else {
         setState(() {
           _error = 'Ciudad no encontrada';
@@ -95,10 +100,34 @@ print(response.body);  // Ver la respuesta completa para detectar posibles error
         });
       }
     } catch (e) {
+      print(e);
       setState(() {
         _error = 'Error al obtener los datos del clima';
         _forecast = [];
       });
+    }
+  }
+
+  // Método para navegar entre las vistas
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // Método para obtener la vista según el índice seleccionado
+  Widget _getCurrentView() {
+    switch (_selectedIndex) {
+      case 0: // Días
+        return _buildForecastView();
+      case 1: // Horas
+        return _buildHourlyForecastView();
+      case 2: // Inicio
+        return _buildTodayView();
+      case 3: // Mapas
+        return _buildMapView();
+      default:
+        return _buildForecastView();
     }
   }
 
@@ -127,6 +156,115 @@ print(response.body);  // Ver la respuesta completa para detectar posibles error
     return iconMap[iconCode] ?? Icons.error;
   }
 
+  // Construir vista de pronóstico diario
+  Widget _buildForecastView() {
+    return Column(
+      children: <Widget>[
+        if (_error.isNotEmpty)
+          Text(_error, style: TextStyle(color: Colors.red, fontSize: 18)),
+        if (_forecast.isNotEmpty)
+          Expanded(
+            child: ListView.builder(
+              itemCount: _forecast.length,
+              itemBuilder: (context, index) {
+                final forecast = _forecast[index];
+                final rain = forecast['rain'] ?? 0.0;
+                final iconCode = forecast['icon'] ?? '';
+
+                return Card(
+                  child: ListTile(
+                    leading: Icon(
+                      _getWeatherIcon(iconCode),
+                      size: 40,
+                      color: Colors.blue,
+                    ),
+                    title: Text(forecast['date']),
+                    subtitle: Text('${forecast['dayOfWeek']}'),
+                    trailing: Text('${forecast['temp']}°C'),
+                    onTap:
+                        rain > 0
+                            ? () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text('Precipitaciones'),
+                                    content: Text(
+                                      rain > 0
+                                          ? 'Precipitaciones: $rain mm'
+                                          : 'No hay precipitaciones',
+                                    ),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text('Cerrar'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            }
+                            : null,
+                  ),
+                );
+              },
+            ),
+          ),
+      ],
+    );
+  }
+
+  // Vista de pronóstico por horas (a implementar)
+  Widget _buildHourlyForecastView() {
+    return Center(child: Text("Pronóstico por horas (Aún no implementado)"));
+  }
+
+  // Vista de información de hoy (A implementar)
+  Widget _buildTodayView() {
+    return Center(
+      child: Text("Información del día de hoy (Aún no implementado)"),
+    );
+  }
+
+  // Vista del mapa
+  // Vista del mapa
+  Widget _buildMapView() {
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: _cityLatLng, // Coordenadas de la ciudad
+        initialZoom: 12.0, // Nivel de zoom inicial
+      ),
+      children: [
+        // Capa de temperatura de OpenWeatherMap
+        TileLayer(
+          urlTemplate:
+              "https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=1bacfbd7cde7607f9441c8e0c8d09a69", // URL de la capa de temperatura
+          subdomains: [
+            'a',
+            'b',
+            'c',
+          ], // Subdominios para distribuir las solicitudes de tiles
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+              point: _cityLatLng, // Ubicación de la ciudad
+              width: 40,
+              height: 40,
+              child: const Icon(
+                Icons.location_pin,
+                color: Colors.red,
+                size: 40,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -139,108 +277,69 @@ print(response.body);  // Ver la respuesta completa para detectar posibles error
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                labelText: 'Ingresa la ciudad',
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (value) {
-                setState(() {
-                  _city = value;
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_city.isNotEmpty) {
-                  _fetchWeather();
-                }
-              },
-              child: const Text('Obtener pronóstico semanal'),
-            ),
-            const SizedBox(height: 20),
-            if (_error.isNotEmpty)
-              Text(
-                _error,
-                style: TextStyle(color: Colors.red, fontSize: 18),
-              ),
-            if (_forecast.isNotEmpty) 
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _forecast.length,
-                  itemBuilder: (context, index) {
-                    final forecast = _forecast[index];
-                    final rain = forecast['rain'] ?? 0.0;
-                    final iconCode = forecast['icon'] ?? '';
-
-                    return Card(
-                      child: ListTile(
-                        leading: Icon(
-                          _getWeatherIcon(iconCode),
-                          size: 40,
-                          color: Colors.blue,
-                        ),
-                        title: Text(forecast['date']),
-                        subtitle: Text('${forecast['dayOfWeek']}'),
-                        trailing: Text('${forecast['temp']}°C'),
-                        onTap: rain > 0 ? () {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: const Text('Precipitaciones'),
-                                content: Text(rain > 0
-                                    ? 'Precipitaciones: $rain mm'
-                                    : 'No hay precipitaciones'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text('Cerrar'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        } : null,
-                      ),
-                    );
-                  },
-                ),
-              ),
-            Expanded(
-              child: FlutterMap(
-                options: MapOptions(
-                  initialCenter: _cityLatLng,
-                  initialZoom: 12.0,
-                ),
+            Visibility(
+              visible: _selectedIndex != 3, // Ocultar en Mapas
+              child: Column(
                 children: [
-                  TileLayer(
-                    urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    subdomains: [],
+                  TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      labelText: 'Ingresa la ciudad',
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _city = value;
+                      });
+                    },
                   ),
-                  MarkerLayer(
-                    markers: [
-                      Marker(
-                        point: _cityLatLng,
-                        width: 40,
-                        height: 40,
-                        child: const Icon(
-                          Icons.location_pin,
-                          color: Colors.red,
-                          size: 40,
-                        ),
+                  const SizedBox(height: 20),
+                  Visibility(
+                    visible:
+                        _selectedIndex == 0 ||
+                        _selectedIndex == 1, // Mostrar solo en Días y Horas
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_city.isNotEmpty) {
+                          _fetchWeather();
+                        }
+                      },
+                      child: Text(
+                        _selectedIndex == 1
+                            ? 'Obtener pronóstico por horas'
+                            : 'Obtener pronóstico semanal',
                       ),
-                    ],
+                    ),
                   ),
+                  const SizedBox(height: 20),
                 ],
               ),
             ),
+            Expanded(child: _getCurrentView()), // Mostrar la vista actual
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.blue, // Color de fondo del menú
+        selectedItemColor: Colors.white, // Color del icono y texto seleccionado
+        unselectedItemColor: Colors.white70, // Color de iconos no seleccionados
+        showUnselectedLabels:
+            true, // Mostrar etiquetas en elementos no seleccionados
+        type:
+            BottomNavigationBarType
+                .fixed, // Evita animaciones molestas en más de 3 ítems
+        elevation: 10, // Agrega sombra al menú
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Días',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.schedule), label: 'Horas'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Mapas'),
+        ],
       ),
     );
   }
